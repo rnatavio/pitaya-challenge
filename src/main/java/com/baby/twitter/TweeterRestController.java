e488f8b647e3f0f8a2116f04d6a37dac10d00ead
Roy Remus <rnatavio@exist.com>
2017-02-22 18:03:19 +0800

    added cors config

--- src/main/java/com/baby/twitter/TweeterRestController.java
+++ src/main/java/com/baby/twitter/TweeterRestController.java
@@ -6,6 +6,7 @@ import javax.inject.Inject;
 import org.springframework.social.connect.ConnectionRepository;
 import org.springframework.social.twitter.api.Tweet;
 import org.springframework.social.twitter.api.Twitter;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,7 +24,8 @@ public class TweeterRestController {
         this.twitter = twitter;
         this.connectionRepository = connectionRepository;
     }
-
+    
+    @CrossOrigin(origins = "https://8080-dot-2269968-dot-devshell.appspot.com")
     @RequestMapping(method=RequestMethod.GET)
     public List<Tweet> showTweets() {
         return twitter.timelineOperations().getUserTimeline("Optus");
